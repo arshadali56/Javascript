8444a395665e247d685ae87769669ff669a6610b
arshadali56 <155185149+arshadali56@users.noreply.github.com>
2023-12-29 20:09:01 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -8,5 +8,5 @@ let accountState;
 accountEmail = "sam@t.com"
 accountPassword = 78945
 accountCity = "Lucknow"
-console.table([accountId,accountEmail,accountPassword,accountCit,accountState]);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
 
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treatv all JS code as newr version
+
+//alert( 3+3 )// we are using node.js, not browser
+
+let name= "arsh"
+let age= 18
+let isLoggedIn= false
+
+
+//number => 2 to powe 53
+//bigint
+//string => ""
+//boolean => true/false
+//null  => standAlone value
+//undefined =>
+//symbol => unique
+
+// object 
+
+console.log(typeof "arsh"); //string
+console.log(typeof null); //object
