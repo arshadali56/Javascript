fb70255656c04a304380d09335e7f1d63316ca99
arshadali56 <155185149+arshadali56@users.noreply.github.com>
2023-12-29 19:42:28 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId =1245
+let accountEmail = "arsh@gmail.com"
+var accountPassword = 54321
+accountCity = "Delhi"
+let accountState;
+
+
+accountEmail = "sam@t.com"
+accountPassword = 78945
+accountCity = "Lucknow"
+console.table([accountId,accountEmail,accountPassword,accountCit,accountState]);
+
