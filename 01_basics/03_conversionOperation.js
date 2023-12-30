7f4398f4ecf53847a62455ead731f52126c19a8d
arshadali56 <155185149+arshadali56@users.noreply.github.com>
2023-12-30 12:26:48 +0000

    container config done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let isLoggedIn = 1
+
+
+let booleanisLoggedIn = boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+// "33" => 33
+// 
